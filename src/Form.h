45a304fd82c333b75699b49ac06f117e4f7af359
Cao Junkai <101247688+UMSCJK@users.noreply.github.com>
2026-09-13 23:41:26 +0300

    feature/move_figures: make m_offset non-const, add setOffset() and use it for tr

--- src/Form.h
+++ src/Form.h
@@ -2,20 +2,22 @@
 
 class Triangle
 {
-    const unsigned int m_offset;
+    unsigned int m_offset;
     const unsigned int m_leg_length;
 
 public:
     Triangle(unsigned int offset, unsigned int leg_length) : m_offset(offset), m_leg_length(leg_length) {}
+    void setOffset(unsigned int offset) { m_offset = offset; }
     void draw() const;
 };
 
 class Circle
 {
-    const unsigned int m_offset;
+    unsigned int m_offset;
     const unsigned int m_radius;
 
 public:
     Circle(unsigned int offset, unsigned int radius) : m_offset(offset), m_radius(radius) {}
+    void setOffset(unsigned int offset) { m_offset = offset; }
     void draw() const;
 };
